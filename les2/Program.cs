3dc239366d644d44da74c7d8395ac6acc79eaf2e
Alex <aisharyshev@gmail.com>
2022-09-09 17:32:59 +0300

    третье число

---
+++ les2/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введите число:  ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int y = 10;
+int count = 1;
+
+if (x < 100 && x > -100)
+{
+    Console.Write("Третьей цифры нет");
+}
+else
+{
+    while (x / y > 99 || x / y < -99)
+    {
+        y = y * 10;
+        count = count * 10;
+    }
+    long result = (x % y) / count;
+    Console.Write($"Третья цифра числа : {Math.Abs(result)}");
+}
