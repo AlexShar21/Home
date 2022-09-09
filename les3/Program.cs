7d2afa6f2d7c93de40c322f2bae69ffa839bfddc
Alex <aisharyshev@gmail.com>
2022-09-09 17:52:53 +0300

    Дни недели

---
+++ les3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Дни недели
+Console.WriteLine("Введите цифру дня недели:  ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+switch(x)
+{
+    case 1:
+        System.Console.WriteLine ("Это понедельник - не выходной.");
+        break;
+    case 2:
+        System.Console.WriteLine("Это вторник - не выходной.");
+        break;
+    case 3:
+        System.Console.WriteLine("Это среда - не выходной.");
+        break;
+    case 4:
+        System.Console.WriteLine("Это четверг - не выходной.");
+        break;
+    case 5:
+        System.Console.WriteLine("Это пятница - не выходной.");
+        break;
+    case 6:
+        System.Console.WriteLine("Это суббота - выходной.");
+        break;
+    case 7:
+        System.Console.WriteLine("Это воскресенье - выходной.");
+        break;
+        }
